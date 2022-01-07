80a9eb831d64615de79753ca16ba79b8db307a7b
suspiciouscouch <sungjinlim.real@gmail.com>
2022-01-07 22:16:46 +0700

    added in leaflet without connection in ip data

--- app.js
+++ app.js
@@ -57,3 +57,18 @@ button.addEventListener("click", async () => {
     alert(`Please enter a valid ip address`);
   }
 });
+
+var map = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3VzcGljaW91c2NvdWNoIiwiYSI6ImNreTRqNnl0eDBjZXoybnE5Y2NybTMwNjEifQ.gEqz2W_1TLNSXcf0XHmVIg",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1Ijoic3VzcGljaW91c2NvdWNoIiwiYSI6ImNreTRqNnl0eDBjZXoybnE5Y2NybTMwNjEifQ.gEqz2W_1TLNSXcf0XHmVIg",
+  }
+).addTo(map);
